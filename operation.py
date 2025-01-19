c352096bc69aea074e84b20538e74ee38018d6bc
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-01-19 18:03:39 +0900

    実装予定の機能完了

--- operation.py
+++ operation.py
@@ -9,8 +9,19 @@ def main(args):
     elif(args[1] != "markdown"):
         print("Unknown that command：" ,args[1])
 
+    #パスの認識
     inputpath = args[2]
     outputpath = args[3]
+
+    #mdファイルの内容の読み込み
+    with open(inputpath, encoding='utf-8') as f:
+        contents = f.read()
+
+    #md to html と　index.htmlファイルとして出力
+    md = markdown.Markdown(extensions=['tables'])
+    changedContents = md.convert(contents)
+    with open(outputpath, "w") as f:
+        f.write(changedContents)
 
 #シェルで　python3 file-converter.py markdown inputfile outputfile
 args = sys.argv
